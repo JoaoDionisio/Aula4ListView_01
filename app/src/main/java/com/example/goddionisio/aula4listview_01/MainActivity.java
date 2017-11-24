a60fc58163dbb2895a82c1d0c74e31b1bfa85115
Joao Dionisio <jd1583@hotmail.com>
2017-11-24 19:37:53 -0300

    Funcionando commit

--- app/src/main/java/com/example/goddionisio/aula4listview_01/MainActivity.java
+++ app/src/main/java/com/example/goddionisio/aula4listview_01/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.goddionisio.aula4listview_01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView minhaListView;
+
+    private ArrayAdapter<String> arrayAdapter;
+
+    String[] disciplinas = {"Matematica", "Portugues", "Fisica", "Quimica", "Ingles",
+                            "Geografia", "Historia", "Informatica", "Musica"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minhaListView = findViewById(R.id.listview);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, disciplinas);
+
+        minhaListView.setAdapter(arrayAdapter);
+
+
     }
 }
